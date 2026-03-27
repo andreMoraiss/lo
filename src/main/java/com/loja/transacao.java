e1b26bb05966d4e423d0b0863833647995180ad6
André Gustavo Queiroz de Morais <andre.gustavo.queiroz.morais@gmail.com>
2026-03-27 19:22:43 -0300

    Create Transacao class to represent a transaction

---
+++ src/main/java/com/loja/transacao.java
@@ -0,0 +1,13 @@
+package com.loja.dominio;
+
+public class Transacao {
+    private double valor;
+
+    public Transacao(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
